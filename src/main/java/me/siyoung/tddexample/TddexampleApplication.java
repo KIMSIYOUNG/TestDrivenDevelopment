b7a27d30c7947fed457d59222fee1c4e444ed838
KIMSIYOUNG <tldud2404@gmail.com>
2019-10-15 13:32:28 +0900

    test

--- src/main/java/me/siyoung/tddexample/TddexampleApplication.java
+++ src/main/java/me/siyoung/tddexample/TddexampleApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TddexampleApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(TddexampleApplication.class, args);
     }
