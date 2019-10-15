00d8b84afadea60e24ee7c2e68d5232c3aaede54
KIMSIYOUNG <tldud2404@gmail.com>
2019-10-15 13:28:18 +0900

    first init

---
+++ src/main/java/me/siyoung/tddexample/TddexampleApplication.java
@@ -0,0 +1,13 @@
+package me.siyoung.tddexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddexampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TddexampleApplication.class, args);
+    }
+
+}
