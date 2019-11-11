9460355efa68d19d4587a6a3260d59a0eb11e9dc
KIMSIYOUNG <tldud2404@gmail.com>
2019-11-11 18:43:26 +0900

    더하기 연산을 위한 사전작업(환율변환)

--- test/Tests.java
+++ test/Tests.java
@@ -51,6 +51,18 @@ public class Tests {
         Money result = bank.reduce(Money.dollar(1),"USD");
         assertEquals(Money.dollar(1),result);
     }
+    @Test
+    public void testIdentityRate(){
+        assertEquals(1,new Bank().rate("USD","USD"));
+    }
+    @Test
+    public void testReduceMoneyDifferentCurrency(){
+        Bank bank = new Bank();
+        bank.addRate("CHF","USD",2);
+        Money result = bank.reduce(Money.franc(2),"USD");
+        assertEquals(Money.dollar(1),result);
+    }
+
 
 
 }
